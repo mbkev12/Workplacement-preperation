7178bfa8fcc9413c1669c48eb66fc6c7198ba92d
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2022-11-15 22:30:27 +0100

    Add files via upload

---
+++ overloading.java
@@ -0,0 +1,10 @@
+// java default method overloading 
+public class StudentCard {
+    static final int DEFAULT_STUDENT_ID = 1;
+    private String Firstname; 
+    private String Secondname;
+    private int StudentID;
+    private static YearMonth;
+    private int year;
+    private int month;
+}
