16192c0caf77f4abe099c5977f41455e09284d33
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2022-11-18 21:20:31 +0100

    Add files via upload

---
+++ AdditionOperator.java
@@ -0,0 +1,7 @@
+    public class Addition{  
+    public static void main(String args[]){  
+    short AdditionA=15;  
+    short AdditionB=10;  
+    Addition=(short)(a+b);
+    System.out.println(a);  
+    }}  
