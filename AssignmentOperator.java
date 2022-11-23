8011f6ab3f70e21821275a204df4b32ac8326f8e
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2022-11-23 19:14:43 +0100

    Update AssignmentOperator.java

--- AssignmentOperator.java
+++ AssignmentOperator.java
@@ -1,16 +1,32 @@
-
 public class TaxCalculations
 { 
     public static void main(String[] args)
     { 
+        
+        //Assignment Operator
        int TaxA = 1000, TaxB = 300, TaxC = 5000; 
+       
+       //Arithmetic Operator
+       int TaxD = 1000, TaxE = 3000, TaxCal;
+       
+      
+    //Assignment Operator
     TaxA += TaxB; 
     TaxB -= TaxA + TaxC; 
     TaxC *= TaxA * TaxB;
- System.out.println(" Tax Number one = " +TaxA ); 
- System.out.println(" Tax Number two = " +TaxB ); 
- System.out.println(" Tax Number Three = " +TaxC ); 
+    
+    //Arithmetic Operators
+    TaxCal=TaxD + TaxE;
+    
+    
+    
+//Assignment Operator
+ System.out.println("TaxA = " +TaxA ); 
+ System.out.println("TaxB = " +TaxB ); 
+ System.out.println("TaxC = " +TaxC ); 
+ 
+ //Arithmetic Operators
+ System.out.println(TaxCal);
  } 
 }
-
 
