b62d916f52bed9836a5e264432ce32c64d13d2ba
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2022-11-25 16:03:23 +0100

    Update SingleCharterinput.java

--- SingleCharterinput.java
+++ SingleCharterinput.java
@@ -1,5 +1,11 @@
-  import java.io.InputStream;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+
+
 
 
 /**
@@ -14,6 +20,12 @@ public class JavaProgramming
       //Creating a Scanner object
     
       Scanner sc = new Scanner(System.in);
+      FileInputStream file = new FileInputStream("input.txt");
+      InputStreamReader input1 = new InputStreamReader(file);
+      BufferedReader in = BufferedReader(r);
+      
+    
+      
       //Creating a StringBuffer object
       System.out.println("Enter Tax Rate: (TaxA, TaxB, TaxC, TaxD)");
       char grade = sc.next().toCharArray()[0];
