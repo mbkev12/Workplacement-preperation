9dd91253890c5f0885ed6342f850e2eb9c5213e2
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2022-12-03 17:34:08 +0000

    Add files via upload

---
+++ NestedIfstatement.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+public class JavaProgramming {
+    
+    public static void main(String args[]){
+
+      int Taxx = 300;
+      int Taxy = 100;
+
+      if( Taxx == 300 ) {
+         if( Taxy == 100 ) {
+            System.out.print("Tax X and Tax Y our equal");
+         }
+      
+}
+      }
+    
+}
+
+      
+      
+      
