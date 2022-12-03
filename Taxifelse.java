20ed18dfb9cb93c7d7a2d7097a10b93e082d126e
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2022-12-03 13:15:52 +0000

    Add files via upload

---
+++ Taxifelse.java
@@ -0,0 +1,18 @@
+public class JavaProgramming {
+  public static void main(String[] args) {
+ 
+     
+    int Hours = 100 ;
+
+   
+    if (Hours > 40) {
+      System.out.println("Your hours has been calculated and is greater than mininum wage you will be taxed at Rate of A ");
+    }
+ 
+    else {
+      System.out.println("Your hours have been calcaled and you will not be taxed");
+    }
+
+   
+  }
+}
