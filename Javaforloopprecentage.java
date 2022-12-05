48de1d9a5a6994694b4321e70c0675e206094194
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2022-12-05 09:45:18 +0000

    Add files via upload

---
+++ Javaforloopprecentage.java
@@ -0,0 +1,14 @@
+public class JavaProgramming {
+
+ public static void main(String[] args) {
+        
+      
+    // create an array
+    int[] Tax = {30, 40, 60, 50};
+    
+    // iterating through the array 
+    for (int Precentages: Tax) {
+       System.out.println(Precentages);
+    }
+  }
+}
