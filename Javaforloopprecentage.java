30443da5ffcb37d9cb647e12384cabf54262acf8
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2022-12-05 09:45:54 +0000

    Update Javaforloopprecentage.java

--- Javaforloopprecentage.java
+++ Javaforloopprecentage.java
@@ -3,10 +3,10 @@ public class JavaProgramming {
  public static void main(String[] args) {
         
       
-    // create an array
+    
     int[] Tax = {30, 40, 60, 50};
     
-    // iterating through the array 
+    
     for (int Precentages: Tax) {
        System.out.println(Precentages);
     }
