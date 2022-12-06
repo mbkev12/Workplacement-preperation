cf9dd218531077cceb124c731c7f69bd924e3739
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2022-12-06 15:54:16 +0000

    Add files via upload

---
+++ Characterjavaacess.java
@@ -0,0 +1,9 @@
+public class JavaProgramming {
+
+    public static void main(String[] args) throws IOException {
+       String str = "Tax";
+         System.out.println("String: "+str);
+         // finding character at 4th position
+         System.out.println("Last Character of Tax  "+str.charAt(2));
+      }
+   }
