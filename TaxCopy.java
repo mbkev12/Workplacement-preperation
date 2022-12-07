516ea7cd27e321adde6e4719e26855c961b4a7f2
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2022-12-07 10:12:04 +0000

    Add files via upload

---
+++ TaxCopy.java
@@ -0,0 +1,12 @@
+public class JavaProgramming {
+
+ public static void main(String[] args) throws IOException {
+ 		String Tax = "Precentage";
+
+		String TaxCopy = Tax;
+
+		Tax = "def";
+		System.out.println(TaxCopy);
+
+	}
+}
