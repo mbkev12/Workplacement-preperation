2a105020f38e8384f1d242e331650e8a1e61d713
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2022-12-07 10:40:19 +0000

    Add files via upload

---
+++ ConcatentateJava.java
@@ -0,0 +1,12 @@
+public class JavaProgramming {
+
+ public static void main(String[] args) throws IOException {
+
+  
+        String Name = "Kevin"; 
+        String Surname = "Mbiyavanga";
+
+        String fullname = Name.concat(Surname); 
+        System.out.println(fullname);
+    }
+}
