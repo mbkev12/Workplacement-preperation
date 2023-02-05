68017567a48da1062993d5cb51d317a98d5085b1
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2023-02-05 15:26:54 +0100

    Add files via upload

---
+++ pyinlineifstatement.py
@@ -0,0 +1,9 @@
+def multiplication(number):
+    return number * number
+
+def multiplicationTwo(number):
+    return number * number * number
+
+number = 3
+output = multiplication(number) if number > 2 else multiplicationTwo(number)
+
