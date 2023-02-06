d6080eb21a8ac4c5f5aa38d8dae67be54144f337
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2023-02-06 16:08:28 +0100

    Add files via upload

---
+++ PythonVarible.py
@@ -0,0 +1,12 @@
+# An integer assignment
+student_institution = "National College of Ireland"
+ 
+# A floating point
+year = 3
+ 
+# A string
+name = "Kevin Mbiyavanga"
+ 
+print(student_institution)
+print(year)
+print(name)
