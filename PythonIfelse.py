451ec6e477b5283437425809af04805958208e6e
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2023-02-07 18:55:08 +0100

    Add files via upload

---
+++ PythonIfelse.py
@@ -0,0 +1,9 @@
+AccountBalance = 10000
+
+if AccountBalance > 0:
+    print('There is Money availabale is this acccount')
+
+else:
+    print('Account Balance is empty ')
+
+print('This statement is always executed')
