ab2f1b25d60b571e89595c9552741323ce10c8ca
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2023-02-07 15:03:31 +0100

    Add files via upload

---
+++ PythonPassbyreference.py
@@ -0,0 +1,13 @@
+def set_list(list):
+    list = ["K", "E", "V"]
+    return list
+ 
+def add(list):
+    list.append("N")
+    return list
+ 
+my_list = ["I"]
+ 
+print(set_list(my_list))
+ 
+print(add(my_list))
