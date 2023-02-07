887dba16ad0b62a55ee3827b991e596e43981a21
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2023-02-07 14:52:49 +0100

    Add files via upload

---
+++ PythonNestedifstatement.py
@@ -0,0 +1,8 @@
+num = 100
+if num >= 0:
+    if num == 0:
+        print("Number equals zero ")
+    else:
+        print("This is number greater then zero")
+else:
+    print("This number is less than the number zero")
